300c3cca03b104630d6a0532b8d8a22ff524e91a
Ola Thoresen <ola@nytt.no>
2024-10-20 13:05:46 +0200

    fix: login logic

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -196,7 +196,9 @@ elif [[ "${SOURCE_REPO_HOSTNAME}" != "${DEFAULT_REPO_HOSTNAME}" ]]; then
     info "the target repository is located in Gitea."
     # TODO fikse logikken her...
     origin=$(git remote get-url origin)
+    info "Adding source repo to tea"
     tea login add --name source --url "${SOURCE_REPO_PREFIX}" --user ${SOURCE_REPO_USER} --password "${SOURCE_REPO_PASS}" --token "${SOURCE_REPO_TOKEN}"
+    info "Adding dest repo to tea"
     tea login add --name target --url "${origin}" --token "${GITHUB_TOKEN}"
     sleep 600
   else
