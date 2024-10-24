85f3d275558c7a23736c0a7d04bb6e0e86837a5d
Ola Thoresen <olen@inni.no>
2024-10-24 10:00:38 +0200

    fix: target repo username

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -234,7 +234,7 @@ function git_init() {
     if [[ "${TARGET_REPO_TYPE}" == "gitea" ]]; then
       base_url=$(echo "${TARGET_REPO}" | cut -d "/" -f 1-3)
       info "Adding target repo ${base_url} to tea"
-      tea login add --name target --url "${base_url}" --user "${GITHUB_USER}" --token "${GITHUB_TOKEN}"
+      tea login add --name target --url "${base_url}" --user "${GITHUB_USER}" --password "${GITHUB_TOKEN}" --token "${GITHUB_TOKEN}"
     fi
   else
     info "the source repository is located within GitHub."
